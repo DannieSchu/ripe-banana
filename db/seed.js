630863edec2e57e0c34a86037520339c00aa8bc8
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-26 16:54:45 -0700

    seed database with actors

--- db/seed.js
+++ db/seed.js
@@ -1,12 +1,19 @@
 const Studio = require('../lib/models/Studio');
+const Actor = require('../lib/models/Actor');
 const chance = require('chance').Chance();
 
-module.exports = async({ studiosToCreate = 5 } = {}) => {
+module.exports = async({ studiosToCreate = 10 }, { actorsToCreate = 100 } = {}) => {
 
   const studioEndings = ['Universal', 'Studios', 'Media', 'Pictures', 'Group', 'Entertainment'];
 
-  await Studio.create([...Array(studiosToCreate)].map(() => ({
+  const studio = await Studio.create([...Array(studiosToCreate)].map(() => ({
     name: `${chance.capitalize(chance.word())} ${chance.pickone(studioEndings)}`,
     address: { city: chance.city(), state: chance.state(), country: chance.country() }
+  })));
+
+  const actor = await Actor.create([...Array(actorsToCreate)].map(() => ({
+    name: chance.name(),
+    dob: chance.birthday(),
+    pob: chance.country({ full: true })
   })));
 };
