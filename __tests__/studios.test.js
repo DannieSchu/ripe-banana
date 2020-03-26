165f8c52177cfc33a90bd00ed66a9d79e63ab528
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-26 16:20:39 -0700

    write failing test to get ids and names for all studios

--- __tests__/studios.test.js
+++ __tests__/studios.test.js
@@ -1,10 +1,10 @@
-require('../db/data-helpers');
+const { getStudio, getStudios } = require('../db/data-helpers');
 
 const request = require('supertest');
 const app = require('../lib/app');
 
 /* Studios Routes
-[] `POST /studios` to create a studio
+[x] `POST /studios` to create a studio
 [] `GET /studios` to get all studios
   Returns [{ _id, name }]
 [] `GET /studios/:id` to get a studio by its id
@@ -17,22 +17,38 @@ describe('studios routes', () => {
       .post('/api/v1/studios')
       .send({
         name: 'Barrington Media',
-        address: { 
-          city: 'Kansas City', 
-          state: 'MO', 
-          country: 'USA' }
+        address: {
+          city: 'Kansas City',
+          state: 'MO',
+          country: 'USA'
+        }
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           name: 'Barrington Media',
-          address: { 
+          address: {
             _id: expect.any(String),
-            city: 'Kansas City', 
-            state: 'MO', 
-            country: 'USA' 
+            city: 'Kansas City',
+            state: 'MO',
+            country: 'USA'
           },
           __v: 0
+        });
+      });
+  });
+
+  it('gets names and ids of all studios', async() => {
+    const studios = await getStudios();
+
+    return request(app)
+      .get('/api/v1/studios')
+      .then(res => {
+        studios.forEach(studio => {
+          expect(res.body).toContainEqual({
+            _id: studio._id,
+            name: studio.name
+          });
         });
       });
   });
