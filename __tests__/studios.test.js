c590c714fcd4291d4bceb24265808fad5ae1e9cc
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-26 14:51:04 -0700

    write failing test for adding a studio

---
+++ __tests__/studios.test.js
@@ -0,0 +1,31 @@
+const { getStudio, getStudios } = require('../db/data-helpers');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+/* Studios Routes
+[] `POST /studios` to create a studio
+[] `GET /studios` to get all studios
+  Returns [{ _id, name }]
+[] `GET /studios/:id` to get a studio by its id
+  Returns { _id, name, address, films: [{ _id, title }] }
+*/
+
+describe('studios routes', () => {
+  it('creates a studio', () => {
+    return request(app)
+      .post('/api/v1/studios')
+      .send({
+        name: 'Barrington Media',
+        address: { city: 'Kansas City', state: 'MO', country: 'USA' }
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Barrington Media',
+          address: { city: 'Kansas City', state: 'MO', country: 'USA' },
+          __v: 0
+        });
+      });
+  });
+});
