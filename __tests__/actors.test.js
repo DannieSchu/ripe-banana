dafb2219c1d27b59aaeb7ef3a8b2b23ae432470d
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-26 17:08:15 -0700

    write failing test to add an actor

---
+++ __tests__/actors.test.js
@@ -0,0 +1,45 @@
+const { getActor, getActors } = require('../db/data-helpers');
+
+const request = require('supertest');
+const app = require('../lib/app');
+
+/* Actors Routes
+[] `POST /actors` to create an actor
+[] `GET /actors` to get all actors
+  Return [{ _id, name }]
+[] `GET /actors/:id` to get an actor by their id
+  Return 
+      {
+        name,
+        dob,
+        pob,
+        films: [{
+          id,
+          title,
+          released
+        }]
+    }
+*/
+
+describe('actors routes', () => {
+  it('creates an actor', () => {
+    return request(app)
+      .post('/api/v1/actors')
+      .send({
+        name: 'Tobias Fünke',
+        dob: Date.now(),
+        pob: 'USA'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          name: 'Barrington Media',
+          dob: expect.any(String),
+          pob: 'USA',
+          __v: 0
+        });
+      });
+  });
+});
+
+
