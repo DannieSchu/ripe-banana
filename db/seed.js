0dda07d3c689ecea9c557312b44f1c46d6980548
Dannie Schumaker <draeschumaker@gmail.com>
2020-03-26 23:23:45 -0700

    seed database with films

--- db/seed.js
+++ db/seed.js
@@ -1,18 +1,19 @@
 const Studio = require('../lib/models/Studio');
 const Actor = require('../lib/models/Actor');
 const Reviewer = require('../lib/models/Reviewer');
+const Film = require('../lib/models/Film');
 const chance = require('chance').Chance();
 
-module.exports = async({ studiosToCreate = 10, actorsToCreate = 25, reviewersToCreate = 50 } = {}) => {
+module.exports = async({ studiosToCreate = 5, actorsToCreate = 25, reviewersToCreate = 50, filmsToCreate = 10 } = {}) => {
 
   const studioEndings = ['Universal', 'Studios', 'Media', 'Pictures', 'Group', 'Entertainment'];
 
-  await Studio.create([...Array(studiosToCreate)].map(() => ({
+  const studio = await Studio.create([...Array(studiosToCreate)].map(() => ({
     name: `${chance.capitalize(chance.word())} ${chance.pickone(studioEndings)}`,
     address: { city: chance.city(), state: chance.state(), country: chance.country() }
   })));
 
-  await Actor.create([...Array(actorsToCreate)].map(() => ({
+  const actor = await Actor.create([...Array(actorsToCreate)].map(() => ({
     name: chance.name(),
     dob: chance.birthday(),
     pob: chance.country({ full: true })
@@ -21,5 +22,12 @@ module.exports = async({ studiosToCreate = 10, actorsToCreate = 25, reviewersToC
   await Reviewer.create([...Array(reviewersToCreate)].map(() => ({
     name: chance.name(),
     company: chance.pickone([chance.company(), chance.domain()])
+  })));
+
+  await Film.create([...Array(filmsToCreate)].map(() => ({
+    title: chance.animal(),
+    studio: studio,
+    released: parseInt(chance.year({ min: 1900, max: 2020 })),
+    cast: [{ role: chance.name(), actor: actor }]
   })));
 };
